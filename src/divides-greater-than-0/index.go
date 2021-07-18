e97861941ed4f4618cb6b1f3f7a08b4552a13ced
mrLuisFer <mrluisfeer@gmail.com>
2021-07-18 18:57:52 -0500

    fix: refactoring code

--- src/divides-greater-than-0/index.go
+++ src/divides-greater-than-0/index.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var n float64
 
-	fmt.Println("Write a number: ")
+	fmt.Print("Write a number: ")
 	fmt.Scanln(&n)
 
 	if (n > 0) {
 		result := n / 2
-
 		fmt.Printf("Result: %f", result)
 	} else {
 		fmt.Printf("The number %f is less than or equal to 0", n)
