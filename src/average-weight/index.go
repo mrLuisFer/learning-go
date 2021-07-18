5fef03a260955d4ae39c3c7b454f1482090a39e9
mrLuisFer <mrluisfeer@gmail.com>
2021-07-18 17:31:09 -0500

    fix: editing some code

--- src/average-weight/index.go
+++ src/average-weight/index.go
@@ -5,19 +5,20 @@ import (
 )
 
 func main() {
-	var array [5]int 
+	// Sets the maximum value of values that can be searched
+	var array [5]int
 
 	var total, count int
- 	fmt.Print("How many numbers you want to enter: ")  
+ 	fmt.Print("How many numbers you want to enter: ")
  	fmt.Scanln(&count)
  	
 	 for i := 0; i < count; i++ {  
   	fmt.Print("Enter value: ")
-		
+
   	fmt.Scanln(&array[i])
   	total += array[i]
  	}
 
- average := total / count
- fmt.Printf("Average is  %d", average)
+	average := total / count
+	fmt.Printf("Average is  %d", average)
 }
