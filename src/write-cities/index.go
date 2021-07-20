9240f7cb27ed797dfcad3becb12efcc02f970e4a
mrLuisFer <mrluisfeer@gmail.com>
2021-07-20 10:04:27 -0500

    feat: create file func added

---
+++ src/write-cities/index.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+)
+
+func createFile(fileName string) {
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString("Texto escrito con golang")
+}
+ 
+func main() {
+	var file = "cities.txt"
+	createFile(file)
+}
