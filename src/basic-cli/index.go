312e82e33b9af934dd801c28e58e20cbed52ee81
mrLuisFer <mrluisfeer@gmail.com>
2021-07-21 22:11:07 -0500

    feat: basic cli added with two sample commands

---
+++ src/basic-cli/index.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func print (param interface{}) {
+	fmt.Print(param)
+	fmt.Printf("\n")
+}
+
+func toLowerCase (text string) string {
+	var textFormatted = strings.ToLower(text)
+	return textFormatted
+}
+
+func main() {
+	print("Insert any of the following commands:")
+
+	var cmds = []string{"help", "add"}
+	for _, cmd := range cmds {
+		fmt.Printf("%v ", cmd)
+	}
+	print(":")
+
+	var command string
+	fmt.Scan(&command)
+	command = toLowerCase(command)
+
+	if (command == "help") {
+		print("Prints this help message")
+	}
+}
